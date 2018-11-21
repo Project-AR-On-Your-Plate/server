aa97fc172b9e7a5aa81c40b761c1cd228dfcd87e
Kate <mhq0612@163.com>
2018-11-21 14:44:22 +0000

    update sms content

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ app.post("/orders", function(req, res) {
               }),
               body: `Hi ${user_name}! Your order (id: ${
                 order.id
-              }) is accepted. It will be ready for collection in 15min. `
+              }) has been accepted. It is ready for collection in 15min. `
             })
             .then(notification => console.log(notification.sid))
             .catch(error => console.log(error.message));
